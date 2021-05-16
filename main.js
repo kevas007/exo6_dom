47346ce48ba02489cc31d4140877a38284563832
kevas007 <kevassiobo@gmail.com>
2021-05-16 12:21:19 +0200

    base

---
+++ main.js
@@ -0,0 +1,15 @@
+let tartine = document.getElementsByClassName('tartine')[0];
+
+console.log(tartine.innerText);
+
+
+
+let toast = document.getElementById('toast');
+
+console.log(toast.innerText);
+
+
+
+let tartine2 =  document.getElementsByClassName('tartine')[1];
+
+console.log(tartine2);
